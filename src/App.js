8b6c73fbaedd30bb646c45a07cdf078104624971
mrIvancicIvica <ivica.ivancic007@gmail.com>
2021-01-13 13:28:19 +0100

    add home component and styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Homepage from "./components/HomePage/Homepage.component";
 function App() {
   return (
     <div className='App'>
-      <h1>ivica ja sam kralj samo kralj</h1>
       <Homepage />
     </div>
   );
